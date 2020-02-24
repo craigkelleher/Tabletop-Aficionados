d3d09ac911e5216aa888389be9e8af04c94cbd5d
Craig Kelleher <32819654+craigkelleher@users.noreply.github.com>
2020-02-24 12:51:47 -0800

    Update tabletop.sql
    Moved Boardgames below PrimaryDesigners and PrimaryPublishers so the foreign key they reference is already created to avoid errors
    Moved UsersBoardgames below Boardgames so the foreign key they reference is already created to avoid errors
    Edited Line 7 14 22 35 & 44 for syntax errors: Added ;
    Edited Line 16 to make the entity plural. was PrimaryPublisher
    Edited Line 28 from publisherName to publisherID in the references. Error was: 1005 Foreign key constraint is incorrectly formed.
    Edited Line 37 & 38 in UsersBoardgames, removed AUTO_INCREMENT since those are foreign keys already being incremented in their respective tables.
    Edited Line 47: fixed spelling mistake (firsName)

--- tabletop.sql
+++ tabletop.sql
@@ -1,50 +1,52 @@
-CREATE TABLE Users (
-    'userID' int(11) NOT NULL AUTO_INCREMENT,
-    'userFirstName' varchar(255) NOT NULL,
-    'userLastName' varchar(255) NOT NULL,
-    'email' varchar(255) NOT NULL,
-    PRIMARY KEY ('userID')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+CREATE TABLE `Users` (
+    `userID` int(11) NOT NULL AUTO_INCREMENT,
+    `userFirstName` varchar(255) NOT NULL,
+    `userLastName` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE UsersBoardgames (
-    'userID' int(11) NOT NULL AUTO_INCREMENT,
-    'boardgameID' int(11) NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY ('userID') REFERENCES 'Users'('userID'),
-    FOREIGN KEY ('boardgameID') REFERENCES 'Boardgames'('boardgameID')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+CREATE TABLE `PrimaryDesigners` (
+    `designerID` int(11) NOT NULL AUTO_INCREMENT,
+    `designerFirstName` varchar(255) NOT NULL,
+    `designerLastName` varchar(255) NOT NULL,
+    PRIMARY KEY (`designerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE Boardgames (
-    'boardgameID' int(11) NOT NULL AUTO_INCREMENT,
-    'boardgameName' varchar(255) NOT NULL,
-    'designerID' int(11) NOT NULL,
-    'publisherName' varchar(255) NOT NULL,
-    'rating' int(11),
-    'releaseDate' DATE,
-    'genre' varchar(255),
-    PRIMARY KEY ('boardgameID'),
-    FOREIGN KEY ('designerID') REFERENCES 'PrimaryDesigners'('designerID'),
-    FOREIGN KEY ('publisherName') REFERENCES 'PrimaryPublishers'('publisherID')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+CREATE TABLE `PrimaryPublishers` (
+    `publisherID` int(11) NOT NULL AUTO_INCREMENT,
+    `publisherName` varchar(255) NOT NULL,
+    `gamesPublished` int(11),
+    `yearEstablished` int(11),
+    PRIMARY KEY (`publisherID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE PrimaryDesigners (
-    'designerID' int(11) NOT NULL AUTO_INCREMENT,
-    'designerFirstName' varchar(255) NOT NULL,
-    'designerLastName' varchar(255) NOT NULL,
-    PRIMARY KEY ('designerID')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+CREATE TABLE `Boardgames` (
+    `boardgameID` int(11) NOT NULL AUTO_INCREMENT,
+    `boardgameName` varchar(255) NOT NULL,
+    `designerID` int(11) NOT NULL,
+    `publisherID` int(11) NOT NULL,
+    `rating` int(11),
+    `releaseDate` DATE,
+    `genre` varchar(255),
+    PRIMARY KEY (`boardgameID`),
+    FOREIGN KEY (`designerID`) REFERENCES `PrimaryDesigners`(`designerID`),
+    FOREIGN KEY (`publisherID`) REFERENCES `PrimaryPublishers`(`publisherID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE PrimaryPublisher (
-    'publisherID' int(11) NOT NULL AUTO_INCREMENT,
-    'publisherName' varchar(255) NOT NULL,
-    'gamesPublished' int(11),
-    'yearEstablished' int(11),
-    PRIMARY KEY ('publisherID')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+CREATE TABLE `UsersBoardgames` (
+    `userID` int(11) NOT NULL,
+    `boardgameID` int(11) NOT NULL,
+    PRIMARY KEY (`userID`,`boardgameID`),
+    KEY `boardgameID` (`boardgameID`),
+    FOREIGN KEY (`userID`) REFERENCES `Users`(`userID`),
+    FOREIGN KEY (`boardgameID`) REFERENCES `Boardgames`(`boardgameID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Query for add a new character functionality with colon : character being used to 
 -- denote the variables that will have data from the backend programming language
 
-INSERT INTO 'Users' ('userFirstName', 'userLastName', 'email') VALUES (':firsName', 
+INSERT INTO 'Users' ('userFirstName', 'userLastName', 'email') VALUES (':firstName', 
 ':lastName', ':email_input');
 
 INSERT INTO 'Boardgames' ('boardgameName', 'publisherName', 'rating', 'genre') VALUES 
@@ -58,5 +60,3 @@ INSERT INTO 'PrimaryPublisher' ('publisherName', 'gamesPublished', 'yearEstablis
 
 SELECT * FROM 'Boardgames' WHERE ('boardgameID' = ':gameID','boardgameName' = ':gameName', 'designerID' = ':designID', 'publisherName' = ':pubName', 'rating' = ':rating',
  'releaseDate' = ':release', 'genre' = ':genre');
-
- 
