f3951377672312714699357b8cf64536857a610d
Craig Kelleher <32819654+craigkelleher@users.noreply.github.com>
2020-03-19 15:24:32 -0700

    Add files via upload

---
+++ db_credentials.py
@@ -0,0 +1,4 @@
+host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+user = 'cs340_kellehec' #CHANGE ME
+passwd = '0127' #CHANGE ME
+db = 'cs340_kellehec' #CHANGE ME
