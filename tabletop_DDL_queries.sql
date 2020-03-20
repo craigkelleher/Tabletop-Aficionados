dc07b5f0818d8f7500151d585fd35275487e57de
Craig Kelleher <32819654+craigkelleher@users.noreply.github.com>
2020-03-19 22:46:44 -0700

    Update tabletop_DDL_queries.sql

--- tabletop_DDL_queries.sql
+++ tabletop_DDL_queries.sql
@@ -32,8 +32,9 @@ DROP TABLE IF EXISTS `Boardgames`;
 CREATE TABLE `Boardgames` (
   `boardgameID` int(11) NOT NULL,
   `boardgameName` varchar(255) NOT NULL,
-  `designerID` int(11) NOT NULL,
-  `publisherID` int(11) NOT NULL,
+  `designerFirstName` varchar(31) NOT NULL,
+  `designerLastName` varchar(31) NOT NULL,
+  `publisherName` varchar(21) NOT NULL,
   `rating` int(11) DEFAULT NULL,
   `releaseDate` date DEFAULT NULL,
   `genre` varchar(255) DEFAULT NULL,
@@ -44,8 +45,11 @@ CREATE TABLE `Boardgames` (
 -- Dumping data for table `Boardgames`
 --
 
-INSERT INTO `Boardgames` (`boardgameID`, `boardgameName`, `designerID`, `publisherID`, `rating`, `releaseDate`, `genre`) VALUES
-(1, 'AZUL', 1, 1, 8, '2017-10-27', 'Family');
+INSERT INTO `Boardgames` (`boardgameID`, `boardgameName`, `designerFirstName`, `designerLastName`, `publisherName`, `rating`, `releaseDate`, `genre`, `description`) VALUES
+(1, 'Azul', 'Emmnaul', 'Price', 'CoolGames', 8, '2020-03-25', 'Family', 'A silly game for the whole family'),
+(2, 'Pandemic', 'Matt', 'Leacock', 'Zman Games', 7, '2019-07-15', 'Medical', 'In Pandemic, several virulent diseases have broken out simultaneously all over the world! The players are disease-fighting specialists whose mission is to treat disease hotspots while researching cures for each of four plagues before they get out of hand.'),
+(3, 'Gloomhaven', 'Isaac', 'Childres', 'Cephalofair Games', 9, '2020-03-06', 'Strategy', 'Gloomhaven is a game of Euro-inspired tactical combat in a persistent world of shifting motives. Players will take on the role of a wandering adventurer with their own special set of skills and their own reasons for traveling to this dark corner of the wo');
+
 
 -- --------------------------------------------------------
 
@@ -95,22 +99,22 @@ INSERT INTO `PrimaryPublishers` (`publisherID`, `publisherName`, `gamesPublished
 -- Table structure for table `Users`
 --
 
-DROP TABLE IF EXISTS `Users`;
-CREATE TABLE `Users` (
-  `userID` int(11) NOT NULL,
-  `userFirstName` varchar(255) NOT NULL,
-  `userLastName` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+#DROP TABLE IF EXISTS `Users`;
+#CREATE TABLE `Users` (
+#  `userID` int(11) NOT NULL,
+#  `userFirstName` varchar(255) NOT NULL,
+#  `userLastName` varchar(255) NOT NULL,
+#  `email` varchar(255) NOT NULL
+#) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Users`
 --
 
-INSERT INTO `Users` (`userID`, `userFirstName`, `userLastName`, `email`) VALUES
-(1, 'Craig', 'Kelleher', 'kellehec@oregonstate.edu'),
-(3, 'Patrick', 'Oh', 'ohp@oregonstate.edu'),
-(4, 'Novia', 'Dela Rosa', 'delarosanovia@gmail.com');
+#INSERT INTO `Users` (`userID`, `userFirstName`, `userLastName`, `email`) VALUES
+#(1, 'Craig', 'Kelleher', 'kellehec@oregonstate.edu'),
+#(3, 'Patrick', 'Oh', 'ohp@oregonstate.edu'),
+#(4, 'Novia', 'Dela Rosa', 'delarosanovia@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -118,17 +122,32 @@ INSERT INTO `Users` (`userID`, `userFirstName`, `userLastName`, `email`) VALUES
 -- Table structure for table `UsersBoardgames`
 --
 
-DROP TABLE IF EXISTS `UsersBoardgames`;
-CREATE TABLE `UsersBoardgames` (
-  `userID` int(11) NOT NULL,
-  `boardgameID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+#DROP TABLE IF EXISTS `UsersBoardgames`;
+#CREATE TABLE `UsersBoardgames` (
+#  `userID` int(11) NOT NULL,
+#  `boardgameID` int(11) NOT NULL
+#) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `UsersBoardgames`
 --
 
-INSERT INTO `UsersBoardgames` (`userID`, `boardgameID`) VALUES
+#INSERT INTO `UsersBoardgames` (`userID`, `boardgameID`) VALUES
+#(1, 1);
+
+-- Table structure for table `UsersBoardgames`
+--
+
+DROP TABLE IF EXISTS `PrimaryPublishersPrimaryDesigners`;
+CREATE TABLE `PrimaryPublishersPrimaryDesigners` (
+  `publisherID` int(11) NOT NULL DEFAULT '0',
+  `designerID` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table `UsersBoardgames`
+--
+
+INSERT INTO `PrimaryPublishersPrimaryDesigners` (`publisherID`, `designerID`) VALUES
 (1, 1);
 
 --
@@ -139,9 +158,7 @@ INSERT INTO `UsersBoardgames` (`userID`, `boardgameID`) VALUES
 -- Indexes for table `Boardgames`
 --
 ALTER TABLE `Boardgames`
-  ADD PRIMARY KEY (`boardgameID`),
-  ADD KEY `designerID` (`designerID`),
-  ADD KEY `publisherID` (`publisherID`);
+  ADD PRIMARY KEY (`boardgameID`);
 
 --
 -- Indexes for table `PrimaryDesigners`
@@ -155,18 +172,21 @@ ALTER TABLE `PrimaryDesigners`
 ALTER TABLE `PrimaryPublishers`
   ADD PRIMARY KEY (`publisherID`);
 
+#ALTER TABLE `PrimaryPublishersPrimaryDesigners`
+#  ADD PRIMARY KEY (`publisherID`,`designerID`);
+
 --
 -- Indexes for table `Users`
 --
-ALTER TABLE `Users`
-  ADD PRIMARY KEY (`userID`);
+#ALTER TABLE `Users`
+#  ADD PRIMARY KEY (`userID`);
 
 --
 -- Indexes for table `UsersBoardgames`
 --
-ALTER TABLE `UsersBoardgames`
-  ADD PRIMARY KEY (`userID`,`boardgameID`),
-  ADD KEY `boardgameID` (`boardgameID`);
+#ALTER TABLE `UsersBoardgames`
+#  ADD PRIMARY KEY (`userID`,`boardgameID`),
+#  ADD KEY `boardgameID` (`boardgameID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -193,8 +213,8 @@ ALTER TABLE `PrimaryPublishers`
 --
 -- AUTO_INCREMENT for table `Users`
 --
-ALTER TABLE `Users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+#ALTER TABLE `Users`
+#  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -203,17 +223,25 @@ ALTER TABLE `Users`
 --
 -- Constraints for table `Boardgames`
 --
-ALTER TABLE `Boardgames`
-  ADD CONSTRAINT `Boardgames_ibfk_1` FOREIGN KEY (`designerID`) REFERENCES `PrimaryDesigners` (`designerID`),
-  ADD CONSTRAINT `Boardgames_ibfk_2` FOREIGN KEY (`publisherID`) REFERENCES `PrimaryPublishers` (`publisherID`);
+#ALTER TABLE `Boardgames`
+#  ADD CONSTRAINT `Boardgames_ibfk_1` FOREIGN KEY (`designerID`) REFERENCES `PrimaryDesigners` (`designerID`),
+#  ADD CONSTRAINT `Boardgames_ibfk_2` FOREIGN KEY (`publisherID`) REFERENCES `PrimaryPublishers` (`publisherID`);
 
---
+
+#ALTER TABLE 'PrimaryPublishersPrimaryDesigners'
+#  ADD CONSTRAINT `PrimaryPublishersPrimaryDesigners_ibfk_1` FOREIGN KEY (`designerID`) REFERENCES `PrimaryDesigners` (`designerID`),
+#  ADD CONSTRAINT `PrimaryPublishersPrimaryDesigners_ibfk_2` FOREIGN KEY (`publisherID`) REFERENCES `PrimaryPublishers` (`publisherID`),
+#COMMIT;
 -- Constraints for table `UsersBoardgames`
 --
-ALTER TABLE `UsersBoardgames`
-  ADD CONSTRAINT `UsersBoardgames_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `Users` (`userID`),
-  ADD CONSTRAINT `UsersBoardgames_ibfk_3` FOREIGN KEY (`boardgameID`) REFERENCES `Boardgames` (`boardgameID`) ON DELETE CASCADE;
-COMMIT;
+#ALTER TABLE `UsersBoardgames`
+#  ADD CONSTRAINT `UsersBoardgames_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `Users` (`userID`),
+#  ADD CONSTRAINT `UsersBoardgames_ibfk_3` FOREIGN KEY (`boardgameID`) REFERENCES `Boardgames` (`boardgameID`) ON DELETE CASCADE;
+#COMMIT;
+
+
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
